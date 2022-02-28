88e409bffb30899e6be4bbf865de615360e20507
Lucia Pereira <luhelenapereira@gmail.com>
2022-02-28 19:12:33 -0300

    adiciona sprite inimigo

---
+++ js/InimigoSprite.js
@@ -0,0 +1,35 @@
+import Sprite from "./Sprite.js";
+
+export default class InimigoSprite extends Sprite {
+  desenhar(ctx, dt) {
+    /*this.quadro =
+      this.quadro > this.poses[this.p].end
+        ? this.poses[this.p].init
+        : this.quadro + this.poses[this.p].vel * dt;*/
+    this.executar(dt);
+    super.desenhar(ctx, dt);
+  }
+
+  escolheAlvo(alvo) {
+    this.alvo = alvo;
+      if (alvo.x < this.x) {
+        this.p = 1;
+      } else {
+        this.p = 3;
+      }
+  }
+
+  controlar(dt) {
+    this.vx = 30 * Math.sign(this.alvo.x - this.x);
+    this.vy = 30 * Math.sign(this.alvo.y - this.y);
+    if (this.vx < 0) {
+      this.p = 1;
+    } else if (this.vx > 0) {
+      this.p = 3;
+    } else if (this.vy < 0) {
+      this.p = 0;
+    } else if (this.vy > 0) {
+      this.p = 2;
+    }
+  }
+}
